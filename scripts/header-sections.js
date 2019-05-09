b9a3c24279a62d594a44abcbe6331281b706be74
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-05-09 15:09:09 -0500

    Menu de secciones funcionando, falta animar el contenido

---
+++ scripts/header-sections.js
@@ -0,0 +1,30 @@
+const sectionOne = document.getElementById("section_one");
+const sectionTwo= document.getElementById("section_two");
+const btnSectionOne = document.getElementById("btn_section_one");
+const btnSectionTwo = document.getElementById("btn_section_two");
+const yellow_bar= document.querySelector(".header-sections-selection");
+window.addEventListener('load', ()=>{
+    
+    sectionTwo.style.display= "none";
+
+    btnSectionOne.addEventListener('click',()=> {
+        //console.log("Haz hecho click en la sección 1")
+        sectionTwo.style.display= "none";
+        sectionOne.style.display= "";
+        yellow_bar.style.left= "";
+        btnSectionOne.className = "header-sections-focus";
+        btnSectionTwo.className = "header-sections-blur";
+        
+        
+    });
+    btnSectionTwo.addEventListener('click', ()=>{
+        //console.log("Haz hecho click en la sección 2")
+        sectionOne.style.display= "none";
+        sectionTwo.style.display= "";
+        yellow_bar.style.left= "11rem";
+        btnSectionOne.className = "header-sections-blur";
+        btnSectionTwo.className = "header-sections-focus";
+    });
+
+
+});
