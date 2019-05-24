d6767546d7ae5ad254b5b52baf7e83465c7bca06
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-05-24 18:38:57 -0500

    Datos JSON agregados, funciona el flujo de datos entre páginas

---
+++ scripts/JSON-DATA.js
@@ -0,0 +1,38 @@
+let DJS= {
+    DJ_SHADOW: {
+        nombre: "DJ Shadow",
+        generos: "IDM / Ambient",
+        biografia: "Loremañska",
+        socialMedia: {
+            spotify: "www.spotify.com",
+            youtube: "www.youtube.com"
+        },
+        avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/DJ_Shadow_tim_festival.jpg/230px-DJ_Shadow_tim_festival.jpg",
+        discos: {
+            Entroducing: {
+                url: "http://127.0.0.1:5500/sections/Disc/one.html",
+                Nombre: "Endtroducing....",
+                generos: "Ambient/Sampling",
+                description: "Endtroducing..... es el álbum debut del productor musical estadounidense DJ Shadow, lanzado el 19 de noviembre de 1996 bajo el sello Mo' Wax. El álbum es conocido por estar compuesto casi en su totalidad de contenido sampleado, la mayoría del cual se originó a partir de varios discos de vinilo obtenidos por DJ Shadow durante sus viajes a las tiendas de música. Endtroducing..... fue producido por Shadow en el lapso de dos años con un mínimo de equipo, sobre todo el sampler Akai MPC60. Al crear la atmósfera general del disco, se esforzó por captar la naturaleza melancólica de sus lanzamientos anteriores para el sello Mo' Wax. El contenido del álbum incluye tanto temperamentales pistas de ritmo lento como canciones optimistas que recuerdan a las primeras influencias de hip hop de DJ Shadow.\n\nEndtroducing..... recibió la aclamación universal de los críticos, que elogiaron el enfoque de DJ Shadow hacia el sampleado y los ritmos que él creó a partir de los samples. Obtuvo altas posiciones en varias listas de fin de año de los mejores discos del 1996. El álbum es considerado una obra decisiva en el movimiento del hip hop instrumental, con las innovadoras técnicas de muestreo y arreglos de DJ Shadow influenciando a otros productores a crear similares obras basadas en muestras. Endtroducing..... ha aparecido desde entonces en las listas de mejores discos de la década —e incluso de todos los tiempos— de varias publicaciones. ",
+                cover:"https://static.spin.com/files/2016/09/endtroducing-640x427.jpg",
+                socialMedia:{
+                    spotify: "wwww.spotify.com",
+                    youtube: "wwww.youtube.com"
+                },
+                canciones: {
+                    cancion_1: {
+                        nombre: 'Best walk forward',
+                        duration: '0.49',
+                        link: 'wwww.spotify.com/gusanito'
+                    },
+                    cancion_2: {
+                        nombre: 'Best jump forward',
+                        duration: '0.80',
+                        link: 'wwww.spotify.com/gusanito'
+                    }
+                } 
+            }
+        }
+    }
+
+}
